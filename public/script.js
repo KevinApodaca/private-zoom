d2127edfc1a327fc2086bb23be85fea2f4715221
Kevin Apodaca <kevin.apodaca7@gmail.com>
2020-12-25 13:26:06 -0700

    :sparkles: showing your own video on fe

--- public/script.js
+++ public/script.js
@@ -1,8 +1,20 @@
 const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
 /* Setup PeerJS library connections */
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001'
+})
+
+/* Render our own video and mute ourselves, but not for others. */
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream)
 })
 myPeer.on('open', id => {
   socket.emit('join-room', ROOM_ID, id)
@@ -12,3 +24,13 @@ myPeer.on('open', id => {
 socket.on('user-connected', userId => {
   console.log(`Heads up, user ${userId} has connected`)
 })
+
+
+/* Adding video stream */
+function addVideoStream(video, stream) {
+  video.srcObject = stream,
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
