c335b08eab5705df288bc2f95a398ea30e77192e
Kevin Apodaca <kevin.apodaca7@gmail.com>
2020-12-25 13:39:13 -0700

    two way connection

--- public/script.js
+++ public/script.js
@@ -18,7 +18,13 @@ navigator.mediaDevices.getUserMedia({
 
   myPeer.on('call', call => {
     call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
   })
+
+
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
