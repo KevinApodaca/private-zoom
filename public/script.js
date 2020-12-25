b68b167773ff6b2312db7f3786da01058e328085
Kevin Apodaca <kevin.apodaca7@gmail.com>
2020-12-25 12:46:24 -0700

    clients can see which users connected

--- public/script.js
+++ public/script.js
@@ -1,2 +1,7 @@
 const socket = io("/")
 socket.emit('join-room', ROOM_ID, 10)
+
+/* Detect when a new user connects */
+socket.on('user-connected', userId => {
+  console.log(`Heads up, user ${userId} has connected`)
+})
