4d8e8de109b0a67c25711c88ffff7d64531e67ad
Kevin Apodaca <kevin.apodaca7@gmail.com>
2020-12-25 11:49:09 -0700

    :sparkles: generates unique chat room id

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const {v4} = require('uuid')
+const {v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/',(req,res) => {
-  res.redirect(`/${roomId}`)
+  res.redirect(`/${uuidV4()}`)
 })
 
 app.get('/:room', (req, res) => {
